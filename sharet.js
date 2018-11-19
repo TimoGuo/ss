9d3e279843a4cf6cac0ee7eea80591db1e6ec652
TimoGuo <1484598828@qq.com>
2018-11-19 15:08:44 +0800

    Update sharet.js

--- sharet.js
+++ sharet.js
@@ -1,6 +1,16 @@
 (function($){
     $(function(){
 // (function(){
+        
+        //倒计时
+        var countDown = $('#times').text();
+        $('.countdown').downCount({
+            date: countDown,
+            offset: +10
+        }, function () {
+            //alert('倒计时结束!');
+        });
+        
       // 选择地址
     $(document).on('click', '.history li', function () {
         $('.history li').removeClass('active');
