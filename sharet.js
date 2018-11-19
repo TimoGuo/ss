a4de18e2057067ff90b193aba2426e293402ba10
TimoGuo <1484598828@qq.com>
2018-11-19 13:05:39 +0800

    Update sharet.js

--- sharet.js
+++ sharet.js
@@ -7,7 +7,7 @@
         $(this).addClass('active');
     });
     
-    $('.history').html('<li>li1</li><li>li2</li><li>li3</li>');
+    $('.history').html('<li>li1</li><li>li2</li><li>li43</li>');
 //     cli();
 
     $("#buyA").click(function () {
