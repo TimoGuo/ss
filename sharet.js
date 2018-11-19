b3b4431b1d442552a954f34c07b7c97fdcbfa33a
TimoGuo <1484598828@qq.com>
2018-11-19 11:44:19 +0800

    Update sharet.js

--- sharet.js
+++ sharet.js
@@ -1,14 +1,17 @@
 
-// 选择地址
-$(document).on('click', '.history li', function () {
-    $('.history li').removeClass('active');
-    $(this).addClass('active');
-});
+ $(function () {
+      // 选择地址
+    $(document).on('click', '.history li', function () {
+        $('.history li').removeClass('active');
+        $(this).addClass('active');
+    });
 
-$("#buyA").click(function () {
-    alert('buyAA');
-});
+    $("#buyA").click(function () {
+        alert('buyAA');
+    });
 
-$('#buyB').click(function () {
-    alert('buyBB');
-});
+    $('#buyB').click(function () {
+        alert('buyBB');
+    });  
+})
+
