78a9146a1095a8c386d61bc33a6f858926c93935
TimoGuo <1484598828@qq.com>
2018-11-19 15:58:34 +0800

    Update sharet.js

--- sharet.js
+++ sharet.js
@@ -12,7 +12,7 @@
         });
        
         
-       $('.history').html('<li>li2</li><li>li3</li>');
+       $('.history').html('<li>li2</li><li>li1</li><li>li3</li>');
 //     cli();
        
       // 选择地址
